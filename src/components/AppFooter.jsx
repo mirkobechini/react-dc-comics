d9411a70bb3a0bab623dd4c251c80e39e0d2078b
mirkobechini <mirkobechini@gmail.com>
2025-09-21 22:24:54 +0200

    feat: enhance AppFooter layout with new social media links and improved styling

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -5,26 +5,48 @@ import shopLocatorComics from '/img/buy-comics-shop-locator.png'
 import subscriptionComics from '/img/buy-comics-subscriptions.png'
 import powerVisaComics from '/img/buy-dc-power-visa.svg'
 
+
+
+import facebookLogo from '/img/footer-facebook.png'
+import periscopeLogo from '/img/footer-periscope.png'
+import pinterestLogo from '/img/footer-pinterest.png'
+import twitterLogo from '/img/footer-twitter.png'
+import youtubeLogo from '/img/footer-youtube.png'
+
 export default function AppFooter() {
     return (
         <footer>
             <div className="container">
                 <div id="buyRow" className="row">
-                    <div className="col">
+                    <div className="col flex">
                         <img src={digitalComics} alt="buyDigitalComics" />
-                        digital comics
+                        <span className='cardsSpan'>
+                            digital comics
+                        </span>
                     </div>
-                    <div className="col">
+                    <div className="col flex">
                         <img src={merchandiseComics} alt="merchandise" />
+                        <span className='cardsSpan'>
+                            dc merchandise
+                        </span>
                     </div>
-                    <div className="col">
-                        <img src={shopLocatorComics} alt="shopLocator" />
-                    </div>
-                    <div className="col">
+                    <div className="col flex">
                         <img src={subscriptionComics} alt="subscriptions" />
+                        <span className='cardsSpan'>
+                            subscription
+                        </span>
                     </div>
-                    <div className="col">
+                    <div className="col flex">
+                        <img src={shopLocatorComics} alt="shopLocator" />
+                        <span className='cardsSpan'>
+                            comic shop locator
+                        </span>
+                    </div>
+                    <div className="col flex">
                         <img src={powerVisaComics} alt="powerVisa" />
+                        <span className='cardsSpan'>
+                            dc power visa
+                        </span>
                     </div>
 
                 </div>
@@ -32,8 +54,18 @@ export default function AppFooter() {
 
                     ciao
                 </div>
-                <div id="signUp" className="row">
-                    ciao
+                <div id="signUpRow" className="row">
+                    <div className="col">
+                        <a href="" id="signUp">sign-up now!</a>
+                    </div>
+                    <div className="col follow">
+                        <a href="" id='followUs'>follow us</a>
+                        <img src={facebookLogo} alt="facebook" />
+                        <img src={twitterLogo} alt="twitter" />
+                        <img src={youtubeLogo} alt="youtube" />
+                        <img src={pinterestLogo} alt="pinterest" />
+                        <img src={periscopeLogo} alt="periscope" />
+                    </div>
                 </div>
             </div>
         </footer>
