196d12ca748dd624130e0394e9531c25cf2f913f
mirkobechini <mirkobechini@gmail.com>
2025-09-21 21:45:01 +0200

    refactor: restructure AppHeader layout and update CSS for improved styling

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,20 +4,24 @@ import headerLogo from '/img/dc-logo.png'
 export default function AppHeader() {
     return (
         <header>
-            <div className="container menu flex">
-                <img src={headerLogo} className='logo' alt="logo DC" />
-                <nav className='flex'>
-                    <a href="">characters</a>
-                    <a href="" className='selected'>comics</a>
-                    <a href="">movies</a>
-                    <a href="">tv</a>
-                    <a href="">games</a>
-                    <a href="">collectibles</a>
-                    <a href="">videos</a>
-                    <a href="">fans</a>
-                    <a href="">news</a>
-                    <a href="">shop</a>
-                </nav>
+            <div className="container">
+                <div className="row nav">
+                    <div className="col-logo">
+                        <img src={headerLogo} className='logo' alt="logo DC" />
+                    </div>
+                    <nav className="col-nav">
+                            <a href="">characters</a>
+                            <a href="" className='selected'>comics</a>
+                            <a href="">movies</a>
+                            <a href="">tv</a>
+                            <a href="">games</a>
+                            <a href="">collectibles</a>
+                            <a href="">videos</a>
+                            <a href="">fans</a>
+                            <a href="">news</a>
+                            <a href="">shop</a>
+                    </nav>
+                </div>
             </div>
         </header>
     )
