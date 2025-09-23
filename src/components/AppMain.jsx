6d645f9391d81ef9e372d204553012a1c6d039a0
mirkobechini <mirkobechini@gmail.com>
2025-09-23 20:37:27 +0200

    feat: enhance AppMain layout with label styling and position adjustments

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,16 +1,16 @@
 
 import BandIcon from './BandIcon'
 
-export default function AppMain(){
-    return(
+export default function AppMain() {
+    return (
         <main>
             <div className="container flex-container jumbotron">
-
             </div>
             <div id='containerMain' className="container">
+                <span className="label">current series</span>
                 <h2>{"--> Content goes here <--"}</h2>
             </div>
-            <BandIcon/>
+            <BandIcon />
         </main>
     )
 }
