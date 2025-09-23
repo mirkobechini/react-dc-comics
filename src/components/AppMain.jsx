01139db039dc27fd811b663474eef914bb50014d
mirkobechini <mirkobechini@gmail.com>
2025-09-23 20:30:56 +0200

    feat: add jumbotron styling and container to AppMain

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,6 +4,9 @@ import BandIcon from './BandIcon'
 export default function AppMain(){
     return(
         <main>
+            <div className="container flex-container jumbotron">
+
+            </div>
             <div id='containerMain' className="container">
                 <h2>{"--> Content goes here <--"}</h2>
             </div>
