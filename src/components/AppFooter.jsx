95a3c3edf699a93758e96a59ad5642c7f3e02337
mirkobechini <mirkobechini@gmail.com>
2025-09-25 20:34:11 +0200

    feat: update AppFooter to dynamically render banner sections for improved maintainability

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -19,7 +19,7 @@ export default function AppFooter() {
             <div className="container flex-container banner">
                 <div className="leftCol">
 
-                    {banner_list.map(menu =>
+                    {banner_list.slice(0, 2).map(menu =>
                         <section className='linkCard' key={menu.id}>
                             <h2>{menu.title} </h2>
                             <ul>
@@ -32,32 +32,18 @@ export default function AppFooter() {
                         </section>
                     )}
                 </div>
-                <section className="linkCard">
-                    <h2>dc</h2>
-                    <ul>
-                        <li>terms of use</li>
-                        <li>privacy policy (new)</li>
-                        <li>ad choices</li>
-                        <li>advertising</li>
-                        <li>jobs</li>
-                        <li>subscriptions</li>
-                        <li>talent workshop</li>
-                        <li>CPSC certificates</li>
-                        <li>ratings</li>
-                        <li>shop help</li>
-                        <li>Contact us</li>
-                    </ul>
-                </section>
-                <section className="linkCard">
-                    <h2>sites</h2>
-                    <ul>
-                        <li>DC</li>
-                        <li>MAD magazine</li>
-                        <li>DC kids</li>
-                        <li>DC universe</li>
-                        <li>DC power visa</li>
-                    </ul>
-                </section>
+                {banner_list.slice(2).map(menu =>
+                    <section className='linkCard' key={menu.id}>
+                        <h2>{menu.title} </h2>
+                        <ul>
+                            {menu.list.map(list_item =>
+                                <li key={list_item.id}>
+                                    <a href={list_item.href} >{list_item.text}</a>
+                                </li>
+                            )}
+                        </ul>
+                    </section>
+                )}
                 <img src={dcLogoBackground} alt="logoBackground" id="logoBg" />
             </div>
             <div className="container flex-container bottom-band">
